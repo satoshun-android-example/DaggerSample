7a1cc9a27a059427c42ea59d74727d0ec12f3b38
SatoShun <shun.sato1@gmail.com>
2020-06-08 02:54:37 +0000

    fix

--- hilt/src/main/java/com/github/satoshun/example/App.kt
+++ hilt/src/main/java/com/github/satoshun/example/App.kt
@@ -1,12 +1,10 @@
 package com.github.satoshun.example
 
 import android.app.Application
-import dagger.hilt.GenerateComponents
-import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
-@GenerateComponents
-@AndroidEntryPoint
+@HiltAndroidApp
 class App : Application() {
   @Inject lateinit var simpleGreeter: SimpleGreeter
   @Inject lateinit var simpleGreeter2: SimpleGreeter2
