c06aeba50c986573b79c05b471951508876e51b1
SatoShun <shun.sato1@gmail.com>
2020-06-22 05:21:57 +0000

    use ApplicationContext

--- hilt/src/main/java/com/github/satoshun/example/SingletonGreeter.kt
+++ hilt/src/main/java/com/github/satoshun/example/SingletonGreeter.kt
@@ -1,10 +1,14 @@
 package com.github.satoshun.example
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SingletonGreeter @Inject constructor() {
+class SingletonGreeter @Inject constructor(
+  @ApplicationContext private val context: Context
+) {
   private var count: Int = 0
 
   fun countUp(): Int {
