ca17763c56271c41977d392c878ea1326e1be6fd
SatoShun <shun.sato1@gmail.com>
2020-09-20 04:47:00 +0000

    use hilt

--- hilt-migration-android/src/main/java/com/github/satoshun/example/sub/SubActivityCounter.kt
+++ hilt-migration-android/src/main/java/com/github/satoshun/example/sub/SubActivityCounter.kt
@@ -1,8 +1,10 @@
 package com.github.satoshun.example.sub
 
 import android.app.Activity
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
+@ActivityScoped
 class SubActivityCounter @Inject constructor(
   private val activity: Activity
 )
--- hilt-migration-android/src/main/java/com/github/satoshun/example/sub/SubFragment.kt
+++ hilt-migration-android/src/main/java/com/github/satoshun/example/sub/SubFragment.kt
@@ -2,11 +2,13 @@ package com.github.satoshun.example.sub
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.github.satoshun.example.R
-import dagger.android.support.DaggerFragment
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-class SubFragment : DaggerFragment(R.layout.main_frag) {
+@AndroidEntryPoint
+class SubFragment : Fragment(R.layout.main_frag) {
   @Inject lateinit var counter: SubActivityCounter
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
